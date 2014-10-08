d327764f418eb9d05bdf3eb06feaa4b95274a48b
Prelang Builder <builder@prelang.com>
2014-10-08 22:18:13 +0000

    Defined validation for attribute 'zip' on model 'Contact'

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ActiveRecord::Base
   validates_formatting_of :email_1, using: :email
   validates_formatting_of :email_2, using: :email
   validates_formatting_of :email_3, using: :email
+  validates_formatting_of :zip, using: :us_zip
 end
